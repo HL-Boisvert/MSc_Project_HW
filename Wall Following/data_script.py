2c62e7cb3c77100841cee2cf0512f8d013da5ef3
Henri-Louis Boisvert <58815892+HL-Boisvert@users.noreply.github.com>
2022-06-27 00:03:35 +0000

    Add files via upload

---
+++ Wall Following/data_script.py
@@ -0,0 +1,40 @@
+import csv
+import time
+import datetime
+import os
+import numpy as np
+
+
+
+class Output:
+	def __init__(self):
+		self.filename = ''
+		self.start_time = datetime.datetime.now()
+		self.initialised = False
+
+	def initialise(self):
+		self.filename = 'output_' + str(time.strftime("%m-%d-%H-%M-%S")) + '.csv'
+		self.start_time = datetime.datetime.now()
+		with open(self.filename, 'w',newline = '') as f:
+			try:
+				writer = csv.writer(f)
+				writer.writerow(['Time', 'Error Angle', 'Velocity', 'Minimum LiDAR Range'])
+				self.initialised = True
+			except csv.Error as e:
+				sys.exit('file {}, line {}: {}'.format(self.filename, writer.line_num, e))
+
+
+	def append(self, lidar_ranges, angle, velocity):
+		min_range = min(np.array(lidar_ranges.ranges))
+		relative_time = (datetime.datetime.now() - self.start_time).total_seconds()
+		data = [str('{:.2f}'.format(relative_time)),str('{:.2f}'.format(angle)), str('{:.2f}'.format(velocity)),str('{:.2f}'.format(min_range))]
+		if self.initialised == False:
+			self.initialise()
+		with open(self.filename, 'a', newline = '') as f:
+			try:
+				writer = csv.writer(f)
+				writer.writerow(data)
+			except csv.Error as e:
+				sys.exit('file {}, line {}: {}'.format(self.filename, writer.line_num, e))
+
+
